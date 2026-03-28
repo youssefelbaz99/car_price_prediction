5acbba2b5bc89c3c9830aee9acd9550cfdb6e897
youssefelbaz99 <youssefelbaz2006@gmail.com>
2026-03-28 23:29:39 +0200

    Eda_added

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DATA_DIR = "data"
-LOG_DIR = "log"
-
-TRAIN_DATA = os.path.join(DATA_DIR, "train.csv")
-TEST_DATA = os.path.join(DATA_DIR, "test.csv")
-MODEL_PATH = "model.pkl"
-LOG_FILE = os.path.join(LOG_DIR, "run.log")
--- enum_file.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ConfigEnum(Enum):
-    RANDOM_STATE = 42
-    TEST_SIZE = 0.2
-    TARGET_COLUMN = 'price' # هنفترض إن عمود السعر اسمه كده
